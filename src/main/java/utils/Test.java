ff2f0cb3a485fdddeadd131c222afaab64276ee9
Fedorov Pavel Aleksandrovich <p.fedorov@tcsbank.ru>
2019-10-14 10:47:39 +0300

    add time in test

--- src/main/java/utils/Test.java
+++ src/main/java/utils/Test.java
@@ -4,13 +4,18 @@ import org.example.service.TestServiceEndpoint;
 import org.example.testservice.FindNumberRequest;
 import org.example.testservice.FindNumberResponse;
 
+import java.util.Date;
+
 
 public class Test {
     public static void main(String[] args) {
         FindNumberRequest request = new FindNumberRequest();
-        request.setN(1236);
+        request.setN(2365);
         TestServiceEndpoint endpoint = new TestServiceEndpoint();
+        long startTime = new Date().getTime();
         FindNumberResponse response = endpoint.findNumber(request);
+        long endTime = new Date().getTime();
+        System.out.println("Ответ получен за " + (endTime - startTime) + " мс");
         System.out.println("code = " + response.getResult().getCode());
         System.out.println("error = " + response.getResult().getError());
         System.out.println("fileNames = " + response.getResult().getFileNames());
